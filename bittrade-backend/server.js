cfcb140aa6abe49a83c037c520c107c9d6d914f0
Davis <your@email.com>
2025-07-30 10:30:04 +0300

    updates

--- bittrade-backend/server.js
+++ bittrade-backend/server.js
@@ -10,7 +10,15 @@ import Message from './models/Message.js';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://bittrade-delta.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB connection
